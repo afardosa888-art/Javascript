d7930ff89d83b47f83a26110657c65879533cb66
afardosa888-art <afardosa888@gmail.com>
2025-11-29 22:33:45 +0300

    first commit

---
+++ chap1_intro/script.js
@@ -0,0 +1,5 @@
+var firstname = "Fardosa";
+var salary = 200;
+var tax = 30;
+var takehome = salary - tax;
+console.log(firstname, salary, tax, takehome);
---
+++ chap2_variables_and_datatypes/script.js
@@ -0,0 +1,12 @@
+var firstname = "Fardosa";
+var salary = 200;
+var isMarried = false
+// var tax = 30;
+// var takehome = salary - tax;
+// console.log(firstname, salary, tax, takehome);
+// var lastname = "Abdirahman"
+// var total = firstname * lastname;
+// console.log(total);
+console.log(typeof firstname);
+console.log(typeof salary);
+console.log(typeof isMarried);
