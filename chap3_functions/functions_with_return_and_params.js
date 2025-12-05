a99dca2c173d05c079dd21892b781a374112c903
afardosa888-art <afardosa888@gmail.com>
2025-12-05 15:19:42 +0300

    i added content for chapter 3 which is about fuctions

---
+++ chap3_functions/functions_with_return_and_params.js
@@ -0,0 +1,33 @@
+// function multiply(num1,num2) {
+//     return num1 * num2; 
+// }
+
+var multiply = function (x, y) {
+    return x * y; 
+}
+
+// anonymous function
+//  function (x, y) {
+//   return x * y;
+// };
+
+
+
+var result = multiply(5, 8);
+console.log("the result is", result);
+
+var result = multiply(4, 5);
+console.log("the result is", result);
+
+var result = multiply(6, 7);
+console.log("the result is", result);
+
+
+var result = multiply(4, 9);
+console.log("the result is", result);
+
+
+
+
+
+
---
+++ chap3_functions/script.js
@@ -0,0 +1,50 @@
+function whatTheBoysLike() {
+  console.log("Boys");
+  console.log("like");
+  console.log("gaming");
+  console.log("coding");
+  console.log("music");
+  console.log("art");
+    console.log("sports");
+  console.log(
+    "......................................................................................................................................."
+  );
+    
+
+}
+
+function whatTheGirlsLike() {
+  console.log("Girls");
+  console.log("like");
+  console.log("fashion");
+  console.log("coding");
+  console.log("music");
+  console.log("reading");
+  console.log("sports");
+
+  console.log(
+    "......................................................................................................................................."
+  );
+    
+}
+
+function whatTheKidsLike() {
+  console.log("Kids");
+  console.log("like");
+  console.log("cartoons");
+  console.log("ice");
+  console.log("cream");
+  console.log("toys");
+  console.log("playing");
+  console.log("outside");
+  console.log("stories");
+  console.log(
+    "......................................................................................................................................."
+  );
+}
+whatTheBoysLike();
+whatTheGirlsLike();
+// whatTheKidsLike();
+whatTheKidsLike();
+whatTheKidsLike();
+whatTheKidsLike();
