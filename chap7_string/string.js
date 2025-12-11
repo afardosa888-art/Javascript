2ce472b56507e53415e48f67f53f8448ea80112a
afardosa888-art <afardosa888@gmail.com>
2025-12-11 21:33:34 +0300

    I have added content for chapter5 about string

---
+++ chap7_string/string.js
@@ -0,0 +1,49 @@
+// let fullName = "Fardosa Abdirahman";   //double  quotes
+// let fullName = 'Fardosa Abdirahman';   //single  quotes
+// let fullName = new string("Fardosa Abdirahman");  //string objects
+
+
+// let firstName = "Fardosa"
+// let lastName = "Abdirahman"
+// let fullName = firstName + " " +  lastName
+// console.log(fullName)
+// let paragraph = " ok \t \t \tI like Mogadishu   \n"    //  escaping characters             
+// let firstPart = paragraph.substring(6,paragraph.length)
+// console.log(paragraph.repeat(20))
+
+// let fullName = "Fardosa Abdirahman"
+
+// console.log(fullName.valueOf())
+
+// console.log(fullName.endsWith("man"))
+
+// console.log(fullName.startsWith("Fardo"))
+
+// console.log(fullName.split(","))
+
+//  console.log(fullName.slice(3,8))
+
+// let search = /abdi/g
+// console.log(fullName.match(search))
+
+// console.log(fullName.includes("dosa"))
+
+// console.log(fullName.lastIndexOf("n"))
+
+// console.log(firstName.indexOf("d"))
+// console.log(firstName.charAt(5))
+
+// firstName = firstName.trim()
+
+// console.log(firstName.length)
+
+// let lastName = "Abdirahman";
+
+// let fullName = `my full name is ${firstName} ${lastName}`  //backticks
+
+
+// fullName = fullName.replace("my", "our");
+// fullName = fullName.toUpperCase()
+// fullName = fullName.toLowerCase()
+
+// console.log(fullName);
