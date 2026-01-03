c6cc137edbcd4c832f193830da1b29251ad1fe3d
afardosa888-art <afardosa888@gmail.com>
2026-01-03 21:09:26 +0300

    I have added content about Dom to chapter10

---
+++ chap10_DOM/script.js
@@ -0,0 +1,25 @@
+// for (let index = 0; index < 50; index++) {
+//   let firstH1 = document.createElement("p");
+//   firstH1.textContent = index + " ; I'm student of software developer!";
+//   firstH1.style.color = "red"
+//   document.body.style.backgroundColor = "yellow";
+//   let firstp = document.getElementsByTagName("wanna-first");
+//   firstp[1].style.fontSize = "40px";
+//   document.body.appendChild(firstH1);
+//   firstp[0].style.color = "red";
+//   firstp[0].style.fontSize = "30px";
+//   firstp[1].style.color = "green";
+// }
+
+// for (let i = 1; i <= 5000; i++) {
+//   let firstH1 = document.createElement("p");
+//   firstH1.textContent = i + ": Hello <br> World!";
+//   firstH1.style.color = "blue";
+//     document.body.style.backgroundColor = "yellow";
+//   let firstp = document.querySelectorAll(" #wanna-first");
+//   firstp[1].style.fontSize = "40px"
+//   document.body.appendChild(firstH1);
+//   firstp[0].style.color = "red"
+//   firstp[0].style.fontSize = "30px"
+//   firstp[1].style.color = "green"
+// }
